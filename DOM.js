8b0ae5ae9d0af2adf9d2bfc02048c2dec8095d6b
Anjaney Naik <anjaneynaik05@gmail.com>
2025-01-05 02:54:06 -0500

    initial commit

---
+++ DOM.js
@@ -0,0 +1,49 @@
+// console.dir(document.body);
+// console.log(document.body);
+
+
+// console.dir(document.body.childNodes[1]);
+
+// document.body.childNodes[1].innerText = "Changes";
+
+// //SELECTING METHODS
+
+// document.getElementById("topic");
+// let para=document.getElementsByClassName("para");
+// console.dir(para);
+
+// // QUERY SELECTOR 
+
+// document.querySelector("#topic");
+// console.log(topic);
+// console.dir(topic);
+
+// let body=document.querySelectorAll("body");
+// console.dir(body);
+// console.log(body);
+
+
+// ChildNodes
+console.dir(document.body.firstChild);
+
+let div=document.querySelector("#topic");
+console.groupCollapsed(topic);
+
+
+// // practice question
+// let h2 = document.querySelector("h2");
+// console.log(h2);
+
+// h2.innerText = h2.innerText + "\tApna College";
+
+
+// let divs= document.querySelectorAll(".class");
+
+// console.log(divs);
+
+// let idx=1;
+// for(div of divs)
+//     {
+//         div.innerText = `new unique value ${idx}`;
+//         idx++;
+//     }
