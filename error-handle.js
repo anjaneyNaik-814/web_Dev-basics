3a86a1e04c42fbf7749b90729f46fa3fca271017
Anjaney Naik <anjaneynaik05@gmail.com>
2025-01-24 22:38:53 -0500

    initial commit

---
+++ error-handle.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 25;
+ console.log("a" ,a);
+ console.log("b" ,b);
+
+ console.log("a+b =" ,a+b);
+ console.log("a+b =" ,a+b);
+    
+ try{
+    console.log("a+b =" ,a+c); //Normal Code
+ }
+ catch(err){    //err is error object 
+    console.log(err);    //error handling 
+ }
+
+ console.log("a+b =" ,a+b);
+ console.log("a+b =" ,a+b);
+ console.log("a+b =" ,a+b);
+ console.log("a+b =" ,a+b);
+ 
+try{
+   console.log("a+b =" ,a+d);
+}
+catch(err){
+   console.log(err);
+}
+
+ console.log("a+b =" ,a+b);
+ console.log("a+b = ",a+b);
